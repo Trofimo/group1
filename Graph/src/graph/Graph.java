1fa5b5977291784fe75dedfdb70bd28cfb45153a
Администратор <@192.168.0.101>
2014-08-04 09:14:50 +0400

    ok

--- Graph/src/graph/Graph.java
+++ Graph/src/graph/Graph.java
@@ -1,56 +1,174 @@
 package graph;
 
+
+import java.util.LinkedList;
+
 public class Graph {
-	static int[][] a = new int[][] { { 1, 1, 1 ,2 },   
-		                             { 1, 1, 2 ,2 }, 
-		                             { 1, 1, 1, 0 },
-			                         { 2, 1, 1, 2 }, 
-			                       };
-
-	static String[] g = new String[100];
-	private static String[] find(int value){
+	
+	int[][] a;
 		
-		int m = 0;
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < a.length; j++) {
 
-				if( ((i - 1) >= 0)
-					&&((j - 1) >= 0)
-					&&((i + 1) < 5)
-					&&((j + 1) < 3)
-					) {
-					if( (a[i][j] == value)&&(
-					    (a[i][j] == a[i][j - 1])
-					  ||(a[i][j] == a[i][j + 1])
-					  ||(a[i][j] == a[i - 1][j])
-					  ||(a[i][j] == a[i + 1][j])) 
-					  
-					  ) 
-					{
-
-						g[m] = Integer.toString(i)+Integer.toString(j);
-						
-						
-					}else continue;
-				} else if(a[i][j]==value){
-					g[m] = Integer.toString(i)+Integer.toString(j);
-				}
-				m++;
+	static boolean[][]b=new boolean[][]{
+		{false,false,false,false},
+		{false,false,false,false},
+		{false,false,false,false},
+		{false,false,false,false},
+		};
+	static LinkedList<String> g =new LinkedList<>();
+	public  LinkedList<String> findUp(int i,int j){
+		
+		
+		while(i!=0){
+				if(b[i][j]!=true){
 				
-			}
-		}
+					if( i == 0){
+						g.add(Integer.toString(i)+Integer.toString(j));
+						b[i][j]=true;
+					}
+					else if
+					
+					(
+						    
+						  (a[i][j] == a[i - 1][j])) 
+						  
+						   
+						{
+
+							
+							g.add(Integer.toString(i-1)+Integer.toString(j));
+							b[i][j]=true;
+							
+							
+						}i--;
+				}
+				}
+					
+		 return g;
+	}
+	public  LinkedList<String> findDown(int i,int j){
+		
+		
+		while(i!=a.length-1){
+				if(b[i][j]!=true){
+				
+					if( i==a.length-1){
+						
+						    g.add(Integer.toString(i)+Integer.toString(j));
+							
+							b[i][j]=true;
+							
+							
+						
+					}
+					else if( 
+						    
+						  
+						  
+						  (a[i][j] == a[i + 1][j])) 
+						  
+						  
+						{
+						    g.add(Integer.toString(i+1)+Integer.toString(j));
+							
+							b[i][j]=true;
+							
+							
+						}i++;
+				
+				}
+				
+				}
+		 return g;
+	}
+	public  LinkedList<String> findLeft(int i,int j){
+		
+		
+		while(j!=0){
+				if(b[i][j]!=true){
+				
+					if(j==0){
+						g.add(Integer.toString(i)+Integer.toString(j));
+						
+					    b[i][j]=true;
+					    }
+					else if
+						    
+						  (a[i][j] == a[i][j - 1])
+						  
+						 
+						{
+						    g.add(Integer.toString(i)+Integer.toString(j-1));
+							
+							b[i][j]=true;
+							
+							
+						}j--;
+				}
+				
+				
+	}
+		
+		 return g;
+	}
+	public  LinkedList<String> findRight(int i,int j){
+		
+		
+		while(j!=a.length-1){
+				if(b[i][j]!=true){
+				
+					if(j==a.length-1){
+						g.add(Integer.toString(i)+Integer.toString(j));
+						
+						b[i][j]=true;
+					}
+					else if
+						    
+						  (a[i][j] == a[i][j + 1])
+						  
+						  
+						{
+						g.add(Integer.toString(i)+Integer.toString(j+1));
+							
+							b[i][j]=true;
+							
+							
+						}j++;
+				}
+					
+				
+		}	
+			
 
 		 return g;
 	}
-	public static void main(String[] args) {
-		int value = 2;
-		g=find(value).clone();
-		for (int k = 0; k < g.length; k++) {
-			if(g[k]!=null){
-			System.out.println("row: " + g[k]);}
-			
-		}
+	
+//	public static void main(String[] args) {
+//		
+//		LinkedList<String>Up=findUp(3,1);
+//		for (Iterator<String> iterator = Up.iterator(); iterator.hasNext();) {
+//			
+//			String string = (String) iterator.next();
+//			System.out.println(string);
+//		}
+//		
+//		LinkedList<String>Down=findDown(0,0);
+//		for (Iterator<String> iterator = Down.iterator(); iterator.hasNext();) {
+//			
+//			String string = (String) iterator.next();
+//			System.out.println(string);
+//		}
+//		LinkedList<String>Left=findLeft(2,0);
+//		for (Iterator<String> iterator = Left.iterator(); iterator.hasNext();) {
+//			
+//			String string = (String) iterator.next();
+//			System.out.println(string);
+//		}
+//		LinkedList<String>Right=findRight(2,0);
+//		for (Iterator<String> iterator = Right.iterator(); iterator.hasNext();) {
+//			
+//			String string = (String) iterator.next();
+//			System.out.println(string);
+//		}
 
-	}
-
+	//}
 }
--- Graph/tests/graph/GraphTest.java
+++ Graph/tests/graph/GraphTest.java
@@ -2,13 +2,64 @@ package graph;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class GraphTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	 private Graph gr;
+	private LinkedList<String>up=new LinkedList<>();
+	private LinkedList<String>d=new LinkedList<>();
+	private LinkedList<String>left=new LinkedList<>();
+	private LinkedList<String>r=new LinkedList<>();
+	@Before
+	public void setup(){
+		gr=new Graph();
+        gr.a = new int[][] { 
+        	{ 1, 1, 1 ,2 },   
+            { 1, 1, 2 ,2 }, 
+            { 1, 1, 1, 0 },
+            { 2, 1, 1, 2 }, 
+          };
+		
 	}
-
+	
+	@Test
+	public void testUp() {
+		up.clear();
+		up.add(0,"21");
+		up.add(1,"11");
+		up.add(2,"01");
+		
+		assertEquals(up, gr.findUp(3,1));
+		
+	}
+	
+	@Test
+	public void testDown() {
+		d.clear();
+		d.add(0,"10");
+		d.add(1,"20");
+		
+		assertEquals(d, gr.findDown(0,0));
+		
+	}
+	
+	@Test
+	public void testLeft() {
+		
+		left.clear();
+		left.add(0,"21");
+		left.add(1,"20");
+		assertEquals(left, gr.findLeft(2,2));
+	}
+	
+	@Test
+	public void testRight() {
+		r.clear();
+		r.add(0,"21");
+		r.add(1,"22");
+		assertEquals(r, gr.findRight(2,0));
+	}
 }

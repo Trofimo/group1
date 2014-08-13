cca3659be4ff7095d92a4a618a22dbdbb71e12ae
Student <Student@student.ru>
2014-08-13 17:55:34 +0400

    ok

--- SourceCounter/src/scounter/SourceCounter.java
+++ SourceCounter/src/scounter/SourceCounter.java
@@ -1,70 +1,86 @@
 package scounter;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/*
- http://kulibaba.net/programming/java/strings
- http://www.helloworld.ru/texts/comp/lang/java/java/09.htm
- http://habrahabr.ru/post/127307/
- http://www.vova-prog.narod.ru/articles/regular_expressions/regular_expressions_examples.html#spaces
- http://www.quizful.net/post/Java-RegExp
- http://www.shtogrin.com/library/web/pcre/doc/pcre/
- http://www.exlab.net/files/tools/sheets/regexp/regexp.pdf
- */
 public class SourceCounter {
-	int result = 0;
 
-	private String getContent(String filePath) throws IOException {
-		FileInputStream input;
+	public int countText(String filePath) throws IOException {
 
-		input = new FileInputStream(filePath);
+		SourceCounter sourceCounter = new SourceCounter();
+		int res = 0;
 
-		byte[] fileData = new byte[input.available()];
+		Scanner sc = new Scanner(new File(filePath));
+		String str = "";
+		boolean b = false;
+		while (sc.hasNextLine()) {
+			str = sc.nextLine();
+			
+			// в начале проверяем /**/
 
-		input.read(fileData);
-		input.close();
-
-		return new String(fileData, "UTF-8");
-	}
-
-	private String removeComments(String str) {
-		Pattern p = Pattern.compile("/\\*[^\\*]*[^/]*/|\\/\\/[^\\n]*");
-		Matcher m = p.matcher(str);
-		str = m.replaceAll("");
-		return str;
-	}
-
-	private String removeSpaces(String str) {
-		return str.replaceAll("[\\s&&[^\r?\n]]+", "")
-				.replaceAll(" ?(\r\n)+", "\r\n").replaceAll(" ?\n+", "\n")
-				.replaceAll("^ ", "");
-	}
-
-	private int countStr(String str) {
-
-		StringTokenizer tokenizer = new StringTokenizer(str);
-
-		while (tokenizer.hasMoreTokens()) {
-
-			if (tokenizer.nextToken().length() != 0) {
-				result++;
+			Pattern p = Pattern.compile("/\\*+");
+			Matcher m = p.matcher(str);
+			// если обнаруживаем открывающийся блок
+			if (m.find()) {
+				b = true;
+				
 			}
 
+			if (b) {
+				// 1.удаляем первую строчку с откр /* фрагмент
+				str = m.replaceAll("");
+				
+				Pattern p1 = Pattern.compile("(^.*[.\\*]+/)");
+				
+				Matcher m1 = p1.matcher(str);
+				// 3.проверяем есть ли закр */
+				if (m1.find()) {
+
+					// 5.если есть то удаляем только до */ включительно
+					str = m1.replaceAll("");
+					b = false;
+
+				}else{str="";}
+
+			}
+			
+			// после этого можно и // вид комментариев проверить
+			p = Pattern.compile("//.+$");
+			m = p.matcher(str);
+			str = m.replaceAll("");
+
+		
+			
+			
+			// чистим всю нашу строчку от всех пробелов чтобы потом можно было
+			// проверить
+			// является ли она полезной*
+			
+			p = Pattern.compile("^\\s+");
+		m = p.matcher(str);
+			str = m.replaceFirst("");
+			
+			p = Pattern.compile("\\s+$");
+			m = p.matcher(str);
+			str = m.replaceAll("");
+			
+			
+			if (!str.equals("")) {
+				
+				System.out.println(str);
+				res++;
+				System.out.println(res);
+			}
 		}
 
-		return result;
-	}
-
-	public int  countText(String filePath) throws IOException {
-		SourceCounter sc = new SourceCounter();
-		String str = sc.getContent(filePath);
-        str = sc.removeComments(str);
-		str = sc.removeSpaces(str);
-		
-		return countStr(str);
+		return res;
 	}
 }
